c71f073ba588b4f866cd5a9ea00bed144f864272
Suraj Poddar <surajku2022@gmail.com>
2022-09-02 14:30:50 +0530

    added frontend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler} = require("./middlewares/errorMiddleware");
 
 const app = express();
+const connectDB = require("./config/db");
+
+app.use(cors());
 dotenv.config();
+connectDB();
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/", (req, res) => {
+app.get("/notes", (req, res) => {
   res.json(notes);
-});
-
-app.get("/api/:id", (req, res) => {
-  const { id } = req.params;
 });
 
 const PORT = process.env.PORT || 5000;
